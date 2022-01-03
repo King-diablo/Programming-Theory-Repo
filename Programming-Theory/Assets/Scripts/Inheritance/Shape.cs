5e2b937a459f0996eaa7a0ef22c274d20f719a4a
kingdiablo <chikelubaobioraeze@gmail.com>
2022-01-03 14:28:22 +0000

    Inheritance scripts created
    created a base class "shape" and three children class

---
+++ Programming-Theory/Assets/Scripts/Inheritance/Shape.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    public string shapeName;
+    public Color color;
+    public Vector3 size;
+}
